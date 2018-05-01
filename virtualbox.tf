d04a63ba4596fe26a32a9aa9990b14ba6f6199be
Ringo De Smet <ringo@de-smet.name>
2018-05-01 22:47:33 +0200

    TF configuration of terra-farm/virtualbox team.

---
+++ virtualbox.tf
@@ -0,0 +1,17 @@
+resource "github_team" "virtualbox" {
+  name        = "virtualbox"
+  description = "Team to manage the virtualbox provider"
+  privacy     = "closed"
+}
+
+resource "github_team_membership" "virtualbox-Cai_Lei" {
+  team_id  = "${github_team.virtualbox.id}"
+  username = "ccll"
+  role     = "maintainer"
+}
+
+resource "github_team_membership" "virtualbox-Ringo_De_Smet" {
+  team_id  = "${github_team.virtualbox.id}"
+  username = "ringods"
+  role     = "maintainer"
+}
