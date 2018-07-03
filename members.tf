6708fa8368c872344a1daea7b84a89ffecb9eb27
Ringo De Smet <ringo@de-smet.name>
2018-07-03 08:57:22 +0200

    Putting all membership under Terraform control.

---
+++ members.tf
@@ -0,0 +1,14 @@
+resource "github_membership" "Cai_Lei" {
+  username = "ccll"
+  role     = "member"
+}
+
+resource "github_membership" "Matthew_Robinson" {
+  username = "mlrobinson"
+  role     = "member"
+}
+
+resource "github_membership" "Paul_Stack" {
+  username = "stack72"
+  role     = "member"
+}
